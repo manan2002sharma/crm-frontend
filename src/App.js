60211e94588855e877179a872ea1caf1a304cdec
manan2002sharma <109142063+manan2002sharma@users.noreply.github.com>
2023-11-30 13:11:08 +0530

    latest message

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import { PrivateRoute } from "./components/private-route/PrivateRoute.comp";
+import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/dashboard/Dashboard.page";
+import { UserVerification } from "./pages/user-verification/UserVerification.page";
+import { Entry } from "./pages/entry/Entry.page";
+import { Registration } from "./pages/registration/Registration.page";
+import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+import { TicketLists } from "./pages/ticket-list/TicketLists.page";
+import { Ticket } from "./pages/ticket/Ticket.page";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Entry />
+          </Route>
+          <Route exact path="/registration">
+            <Registration />
+          </Route>
+          <Route exact path="/verification/:_id/:email">
+            <UserVerification />
+          </Route>
+
+          <PrivateRoute path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
+          <PrivateRoute path="/add-ticket">
+            <AddTicket />
+          </PrivateRoute>
+          <PrivateRoute path="/tickets">
+            <TicketLists />
+          </PrivateRoute>
+          <PrivateRoute path="/ticket/:tId">
+            <Ticket />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import ticketsReducer from "./pages/ticket-list/ticketsSlice";
+import loginReducer from "./components/login/loginSlice";
+import userReducer from "./pages/dashboard/userSlice";
+import newTicketReducer from "./components/add-ticket-form/addTicketSlicer";
+import registrationReducer from "./components/registration-form/userRegestrationSlice";
+
+const store = configureStore({
+  reducer: {
+    tickets: ticketsReducer,
+    login: loginReducer,
+    user: userReducer,
+    openTicket: newTicketReducer,
+    registration: registrationReducer,
+  },
+});
+
+export default store;
